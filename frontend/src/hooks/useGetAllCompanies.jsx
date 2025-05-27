d6aba41a7957fc0b2f126e2757ece52a8c6ce4bc
Jaydeep Yadav <yadavjay374@gmail.com>
2025-05-27 11:13:11 +0530

    Fixed import in Hook

--- frontend/src/hooks/useGetAllCompanies.jsx
+++ frontend/src/hooks/useGetAllCompanies.jsx
@@ -3,6 +3,7 @@ import { COMPANY_API_END_POINT } from '../utils/constant.js'
 import axios from 'axios'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { toast } from 'sonner'
 
 const useGetAllCompanies = () => {
     const dispatch = useDispatch();
