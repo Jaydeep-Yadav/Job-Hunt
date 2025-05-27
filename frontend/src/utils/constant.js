dd6dc1dd8a81c91eb805a12bbcc61ef8cff45953
Jaydeep Yadav <yadavjay374@gmail.com>
2025-05-27 10:39:05 +0530

    Add .env in frontend

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = import.meta.env.VITE_API_BASE || "http://localhost:8000";
 
 export const USER_API_END_POINT = BASE_URL + "/api/v1/user";
 export const JOB_API_END_POINT = BASE_URL + "/api/v1/job";
